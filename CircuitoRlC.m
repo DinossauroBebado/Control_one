8377d1edda9f68bbdb06b8caf6d06e55934771f3
DinossauroBebado <guipsilva32@gmail.com>
2022-08-25 12:45:59 -0300

    last class

--- CircuitoRlC.m
+++ CircuitoRlC.m
@@ -1,1 +1,23 @@
 %tutorial matlab MIchigan
+J = 0.01;
+b = 0.1;
+K = 0.01;
+R = 1;
+L = 0.5;
+%s = tf('s');
+%P_motor = K/((J*s+b)*(L*s+R)+K^2);
+
+%step(P_motor); 
+
+%step(feedback(P_motor,1));
+%step(feedback(10*P_motor,1));
+hold
+
+help ss2tf 
+
+help tf2ss 
+num = [0.01];
+
+den = [ 0.005,0.06,0.1];
+
+[A,B,C,D] = tf2ss(num,den); 
